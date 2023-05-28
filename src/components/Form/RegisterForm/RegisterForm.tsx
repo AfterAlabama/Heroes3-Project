98718835231123f475811040754bdae711866e78
Pavelingus <zarovnpasha@mail.ru>
2023-05-28 11:05:42 +0300

    register form onload animation added

--- src/components/Form/RegisterForm/RegisterForm.tsx
+++ src/components/Form/RegisterForm/RegisterForm.tsx
@@ -2,8 +2,10 @@ import BackgroundImage from '../../Common/BackgroundImage/BackgroundImage';
 import regPic from '../../../assets/authPic.jpg';
 import RegisterFormCard from './RegisterFormCard';
 import { FormContainer } from '../../../styles/FormContainer';
+import { useCallOnloadAnimation } from '../../../hooks/useCallOnloadAnimation';
 
 const RegisterForm = () => {
+	useCallOnloadAnimation()
 	return (
 		<FormContainer>
 			<BackgroundImage
